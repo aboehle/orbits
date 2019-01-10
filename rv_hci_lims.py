1ddc2b810586790ae9f59e5ae79293863b3145f7
Anna Boehle <anna.boehle@gmail.com>
2019-01-10 14:33:16 +0100

    Loop of m/a, added plot, added function to get rv epochs from the time series downloaded from DACE.

--- rv_hci_lims.py
+++ rv_hci_lims.py
@@ -1,32 +1,98 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+from matplotlib import cm,colors
 import scipy
 from astropy.io import ascii
+import h5py
+import glob
 
 import orbits.kepler as k
+from plotting.plot_contrastcurve import calc_limitingmass
 
+#rootDir = '/data/ipa/meyer/boehlea/data/'
+rootDir = '/Volumes/ipa/meyer/boehlea/data/'
+
+dataDirs = {'tau_ceti': 'tau_ceti_lprime/data_with_raw_calibs_newpipeline/',
+            '40_eri': '40eri_lprime_081116/data_with_raw_calibs/',
+            'axmic': 'axmic_lprime_091011/data_with_raw_calibs/',
+            'kapteyns': 'kapteyns_lprime_081116/data_with_raw_calibs_fmenti/',
+            'hd36395':'hd36395_lprime_081116/data_with_raw_calibs_fmenti/',
+            'hd42581':'hd42581_lprime_081116/data_with_raw_calibs/'}
+
+def get_rv_epochs(star):
+    
+    rvDir = '/Users/annaboehle/research/data/rv/'
+
+    file_ls = glob.glob('{:s}{:s}/{:s}_resid*.rdb'.format(rvDir,star,star))
+    rv_epochs = []
+    for f in file_ls:
+        rv_tab = ascii.read(f)
+        #rv_epochs.append(ascii.read(f))
+        rv_epochs.extend(rv_tab['rjd'][1:])
+
+    return np.array(rv_epochs,dtype=float) + 2400000 
+    
+def get_f_masslimvsep(star, d_star, age_star, mag_star, stack, n):
+
+    # get contrast limits versus projected sep for star
+    f = h5py.File(rootDir + dataDirs[star] + 'PynPoint_database.hdf5','r')
+    
+    contrast_tag_inner = "{:s}_lprime_contrast_limits_stack{:02d}_pca{:02d}_0.1delr".format(star, stack, n)
+    contrast_tag_outer = "{:s}_lprime_contrast_limits_stack{:02d}_adi".format(star, stack)
+    seps = np.concatenate((f[contrast_tag_inner][:,0],f[contrast_tag_outer][:,0]))
+    contr = np.concatenate((f[contrast_tag_inner][:,1],f[contrast_tag_outer][:,1]))
+    
+    f.close()
+
+    # convert contrast limits to mass limits
+    mass_lims = np.zeros(len(contr))
+    for cc in range(len(contr)):
+        mass_lims[cc],t_model,below,above,t_eff   = calc_limitingmass(d_star, age_star, mag_star, contr[cc], filt="L'", interp_age=True)        
+    
+    f_masslimvsep = scipy.interpolate.interp1d(seps,mass_lims,bounds_error=False,fill_value=np.inf)  # outside bounds planets cannot be detected!
+
+    return f_masslimvsep, seps
+    
 def sample_orbits():
 
-    # get star info
-    star = 'hd_36395'
+    # read in info tables
     star_info = ascii.read('/Users/annaboehle/research/code/directimaging_code/plotting/nearest_solar_dist_age.txt')
-    row = np.where(star_info['object_name'] == star)[0][0]
+    contrInfo = ascii.read('/Volumes/ipa/meyer/boehlea/my_papers/archival_stars/tables/contr_info.dat')
+    obsInfo = ascii.read('/Volumes/ipa/meyer/boehlea/my_papers/archival_stars/tables/NACO_obs.dat')
+    
+    # get star info
+    star = 'hd36395'
+    print star
 
+    row_contr = np.where(contrInfo['name'] == star)[0][0]
+    
+    stack, p = contrInfo['stack'][row_contr],contrInfo['percen_frames'][row_contr]
+    cent_size = contrInfo['cent_size'][row_contr]
+    
+    row = np.where(star_info['object_name'] == contrInfo['plt_contr_name'][row_contr])[0][0]
+    
     d_star = star_info['dist'][row]
-    age_star = (star_info['age_min'][row] + star_info['age_max'][row])/2.  # change this to actual best estimate for age
+    age_star = star_info['age'][row]
     mag_star = star_info['mag_star'][row]
     t_baseline = star_info['t_baseline'][row]
     m_star = star_info['m_star'][row]
     rv_std = star_info['rv_std'][row]
-    
-    t_rv = np.array([420.0041, 6640.9533]) + 2450000.
-    t_hci = np.array([2454786.73611])
 
+    row_obs = np.where(obsInfo['name'] == star)[0][0]
+    
+    t_rv = get_rv_epochs(star)
+    t_hci = obsInfo['time'][row_obs]
+    n = int(np.round(p*obsInfo['nframes'][row_obs]/stack))
+
+    # get mass limits v projected sep for star
+    f_masslimvsep, seps = get_f_masslimvsep(star, d_star, age_star, mag_star, stack, n)
+    
     # fix orbital parameters to 0
     e, o, t0 = 0., 0., 0.
 
     # sample i and w
-    nsamples = 10000
+    nsamples = 100
 
     unif = np.random.uniform(size=nsamples)
     i = np.arccos(unif)*(180./np.pi)
@@ -34,41 +100,67 @@ def sample_orbits():
     w = np.random.uniform(size=nsamples, low=0, high=360.)
 
     # for a and m_p
-    a = 21. # AU
-    m_p = 5. # M_J
+    del_m_p = 3.0
+    del_a = 2.
+    m_p_arr = np.arange(14.5, 50, del_m_p)
+    a_arr = np.arange(np.min(seps)*d_star, np.max(seps)*d_star,del_a)
 
-    # convert a to p
-    P = np.sqrt(a**3./(m_star+m_p*954.7919e-6))*365.
-
-    # solve kepler's equation
-    E_rv = k.solve_kepler_eqn(t_rv, e, P, t0)
-    E_hci = k.solve_kepler_eqn(t_hci, e, P, t0)
+    # set up plot
+    fig = plt.figure(figsize=(12,5))
+    ax=plt.subplot(111)
+    norm = colors.Normalize(vmin=0,vmax=100)
+    scalar_map = cm.ScalarMappable(norm=norm,cmap=cm.Blues)
     
-    # get rv prediction
-    x_rv, y_rv, rv_rv = k.orbit_solution(E_rv,
+    for a in a_arr:
+        for m_p in m_p_arr:
+            #a = 15. # AU
+            #m_p = 18. # M_J
+        
+            # convert a to p
+            P = np.sqrt(a**3./(m_star+m_p*954.7919e-6))*365.
+
+            # solve kepler's equation
+            E_rv = k.solve_kepler_eqn(t_rv, e, P, t0)
+            E_hci = k.solve_kepler_eqn(t_hci, e, P, t0)
+    
+            # get rv prediction
+            x_rv, y_rv, rv_rv = k.orbit_solution(E_rv,
                                          e=e, P=P, t0=t0, i=i, w=w, o=o, m_p=m_p, m_star=m_star,d_star=d_star,
                                          units = 'arcsec',
                                          solve_kepler=False)
-
-    print x_rv.shape
     
-    # find astrometric location for these parameters
-    x_ast, y_ast, rv_ast = k.orbit_solution(E_hci,
+            # find astrometric location for these parameters
+            x_ast, y_ast, rv_ast = k.orbit_solution(E_hci,
                                             e=e, P=P, t0=t0, i=i, w=w, o=o, m_p=m_p, m_star=m_star,d_star=d_star,
                                             units = 'arcsec',
                                             solve_kepler=False)
-    print x_ast.shape
 
+            # get projected separations
+            proj_sep = (x_ast.flatten()**2. + y_ast.flatten()**2.)**0.5
+            mass_lims = f_masslimvsep(proj_sep)
+    
+            frac_detected_hci = len(np.where(m_p > mass_lims)[0])/float(nsamples)
 
-    # get projected separations
-    sep = (x_ast.flatten()**2. + y_ast.flatten()**2.)**0.5
+            #plt.scatter(a, m_p)
+            rect = Rectangle( (a-del_a/2.,m_p-del_m_p/2.),
+                              del_a,del_m_p,
+                              facecolor=scalar_map.to_rgba(frac_detected_hci*100.),edgecolor='black')
+            ax.add_patch(rect)
+            plt.scatter(a,m_p)
+            
+            plt.text(a, m_p, '{:2.0f}'.format(frac_detected_hci*100.),
+                     verticalalignment='center',horizontalalignment='center')
 
+            # ultimately: frac detected by one method OR the other
+            
+    plt.show()        
+                            
     # get max delta v in the rv time window
 
-    plt.figure()
-    plt.xlabel('Projected separation')
-    plt.hist((x_ast.flatten()**2. + y_ast.flatten()**2.)**0.5)
-    plt.figure()
-    plt.xlabel('RV change')
-    plt.hist(rv_rv[0] - rv_rv[1])
-    plt.show()
+    #plt.figure()
+    #plt.xlabel('Projected separation')
+    #plt.hist((x_ast.flatten()**2. + y_ast.flatten()**2.)**0.5)
+    #plt.figure()
+    #plt.xlabel('RV change')
+    #plt.hist(rv_rv[0] - rv_rv[1])
+    #plt.show()
